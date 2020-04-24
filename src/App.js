3938a37eefad7190779647957adadde715620176
wymerkd <wymerkd@gmail.com>
2020-04-24 16:23:26 -0700

    add a navbar and test component, import navbar into App.js, routing check

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 // dependencies
 import { Switch, Route } from 'react-router-dom';
 
+//components
+import Test from './components/Test';
+import NavBar from './components/NavBar';
+
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Switch>
-        
+        <Route path='/test' component={Test}/>
       </Switch>
     </div>
   );
